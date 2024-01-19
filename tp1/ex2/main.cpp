08bab558acb55165116002c64a7bd8115d5658ef
maxime.jaillard <maxime.jaillard@orange.fr>
2024-01-19 12:00:46 +0100

    tp1 - ex2 - q6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <vector>
 
 #include "Person.hpp"
 
 int main()
 {
-    Person person {"Palluche", "La Faluche"};
-    std::cout << "La personne s'appelle " << person.getFirstName() << " " << person.getSurname() << std::endl;
+    int nb_persons;
+    std::cout << "Combien de personnes ? ";
+    std::cin >> nb_persons;
+
+    std::vector<Person> persons {};
+    std::string fname, sname;
+    for (auto i = 0; i < nb_persons; i++)
+    {
+        std::cin >> fname >> sname;
+        persons.emplace_back(Person {fname, sname});
+    }
+    
+    for (auto person : persons)
+    {
+        std::cout << "La personne s'appelle " << person.getFirstName() << " " << person.getSurname() << std::endl;
+    }
 
     return 0;
 }
